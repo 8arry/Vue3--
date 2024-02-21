bfe1a4ae8473dd53e6f75acab1884af00ec6a866
8arry <1173309602@qq.com>
2024-02-21 13:10:35 +0800

    一级分类-按照业务逻辑拆分函数

---
+++ src/views/Category/composables/useCategory.js
@@ -0,0 +1,29 @@
+//封装分类数据业务代码
+import { ref, onMounted } from "vue";
+import { getCategoryAPI } from '@/apis/category'
+import { useRoute } from 'vue-router'
+import { onBeforeRouteUpdate } from 'vue-router'
+
+export function useCategory() {
+    const categoryData = ref({})
+    const route = useRoute()
+    const getCategory = async (id = route.params.id) => {
+        const res = await getCategoryAPI(id)
+        console.log(res)
+        categoryData.value = res.result
+    }
+
+onMounted(()=> getCategory())
+
+//目标：路由参数变化的时候，可以把分类数据接口重新发送
+//获取banner
+    onBeforeRouteUpdate((to) => {
+    console.log('路由变化了')
+    //存在问题：使用最新的路由参数请求最新的分类数据
+    getCategory(to.params.id)
+    })
+
+    return {
+        categoryData
+    }
+}
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,44 +1,13 @@
 <script setup>
-import {getCategoryAPI} from '@/apis/category'
-import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
-import { getBannerAPI } from '@/apis/home'
+import { useCategory } from './composables/useCategory'
 import GoodsItem from '../Home/components/GoodsItem.vue'
-import { onBeforeRouteUpdate } from 'vue-router'
+import { useBanner } from './composables/useBanner'
 
+const { bannerList } = useBanner()
+const { categoryData } = useCategory()
 //获取数据
-const categoryData = ref({})
-const route = useRoute()
-const getCategory = async (id = route.params.id) => {
-    const res = await getCategoryAPI(id)
-    console.log(res)
-    categoryData.value = res.result
-}
 
-onMounted(()=> getCategory())
 
-//目标：路由参数变化的时候，可以把分类数据接口重新发送
-//获取banner
-onBeforeRouteUpdate((to) => {
-  console.log('路由变化了')
-  //存在问题：使用最新的路由参数请求最新的分类数据
-  getCategory(to.params.id)
-})
-
-const bannerList = ref([])
-
-const getBanner = async () => {
-    const res = await getBannerAPI({
-        distributionSite: '2'
-    })
-    console.log(res);
-    bannerList.value = res.result
-
-}
-
-onMounted(() => {
-     getBanner()
-})
 
 </script>
 
