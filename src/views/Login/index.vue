f40f64f1cb063ddb82e63f157a997ba6f453049b
8arry <1173309602@qq.com>
2024-02-22 17:08:04 +0800

    登录-pinia管理用户数据

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,8 +4,13 @@
 //表单校验(账号名+密码)
 
 import { ref } from "vue";
+import 'element-plus/theme-chalk/el-message.css'
+import { ElMessage } from "element-plus";
+import { useRouter } from 'vue-router'
 
+import { useUserStore } from '@/stores/user'
 
+const useStore = useUserStore()
 //1.准备表单对象
 const form = ref({
     account: '',
@@ -41,15 +46,22 @@ const rules = {
 
 //3.获取form实例做统一校验
 const formRef = ref(null)
+const router = useRouter()
 const doLogin = ()=> {
+    const { account, password } = form.value
     //调用实例方法
-    formRef.value.validate((valid) => {
+    formRef.value.validate(async (valid) => {
         //valid:所有表单都通过校验才为true
 
         console.log(valid);
         //以valid作为判断条件 如果通过才执行登录逻辑
         if(valid){
             //TODO Login
+            await useStore.getUserInfo({account, password})
+            //1.提示用户
+            ElMessage({type:'success', message:'登录成功'})
+            //2.跳转首页
+            router.replace({path:'/'})
         }
     })
 }
