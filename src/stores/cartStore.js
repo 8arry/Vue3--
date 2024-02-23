c772cedf8ce1dea20362b4662391c4fecc1535c4
8arry <1173309602@qq.com>
2024-02-23 15:11:26 +0800

    登录时合并购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -102,7 +102,8 @@ export const useCartStore = defineStore('cart', ()=>{
         allCheck,
         selectedCount,
         selectedPrice,
-        clearCart
+        clearCart,
+        updateNewList
     }
 },{
     persist: true,
